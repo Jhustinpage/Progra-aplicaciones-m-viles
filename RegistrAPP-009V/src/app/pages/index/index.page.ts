91fa4da301c852f4410d9faae3eb9c9576e3b692
Alex <al.cayuqueo@duocuc.cl>
2024-10-12 22:29:54 -0300

    le agregamos storage

--- RegistrAPP-009V/src/app/pages/index/index.page.ts
+++ RegistrAPP-009V/src/app/pages/index/index.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { UserData } from 'src/app/models/user-data';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-index',
@@ -18,19 +20,33 @@ export class IndexPage implements OnInit {
 
   constructor(
     private router: Router,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private storageService: StorageService
   ) {
     const state = this.router.getCurrentNavigation()?.extras.state;
     if(state){
       console.log(`Username: ${state['user']}`)
       this.username = state['user']
     }
-    this.edLevels.set('0', 'Ing. informatica ');
-    this.edLevels.set('1', 'Analista programador');
-    this.edLevels.set('2', 'Animacion digital');
+    this.edLevels.set('0', 'Basica');
+    this.edLevels.set('1', 'Media');
+    this.edLevels.set('2', 'Superior');
   }
 
   ngOnInit() {
+    const promise = this.storageService.get(`user_data_${this.username}`);
+    console.log("promise: ", promise);
+    if(promise !== undefined){
+      promise.then((data) => {
+        console.log("user_data: ", data);
+        if(data !== null){
+          this.name = data.name;
+          this.lastname = data.lastname;
+          this.edLevel = data.edLevel;
+          this.birthday = data.birthday;
+        }
+      });
+    }
   }
 
   clean(){
@@ -38,6 +54,16 @@ export class IndexPage implements OnInit {
     this.lastname = '';
     this.edLevel = '';
     this.birthday = '';
+  }
+
+  save() {
+    const userData = new UserData(this.name, this.lastname, this.edLevel, this.birthday);
+    const promise = this.storageService.set(`user_data_${this.username}`, userData);
+    if(promise !== undefined){
+      promise.then(() => {
+        console.log('Datos guardados')
+      })
+    }
   }
 
   async showAlert(){
@@ -49,3 +75,4 @@ export class IndexPage implements OnInit {
     alert.present();
   }
 }
+
